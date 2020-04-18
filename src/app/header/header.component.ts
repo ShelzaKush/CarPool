de57139c35470d6470148c885778dc97c2bb44bb
Shelza Kushwah <shelza.kushwah@3SCSOLUTION.LAN>
2020-04-18 22:16:10 +0530

    home_page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-    
+    $(document).ready(function(){
+      $(window).scroll(function() { 
+        if ($(document).scrollTop() > 500) { 
+          $("#nav").css("background-color","#17a2b8");
+        } else {
+          $("#nav").css("background-color", "transparent"); 
+        }
+      });
+    });
   }
+ 
 
 }
